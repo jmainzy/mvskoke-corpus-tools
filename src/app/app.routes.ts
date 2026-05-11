ae5dd01f1ce3664fa8d0b133f6bce48d060a4a20
Julia <juliamainzinger@gmail.com>
2026-05-11 14:48:42 +0700

    Add search page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SearchResultsComponent } from './search/search.component';
 import { SpeechRecognitionComponent } from './speech-recognition/speech-recognition.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'search', component: SearchResultsComponent },
     { path: 'speech-recognition', component: SpeechRecognitionComponent },
     { path: '**', redirectTo: '' }
 ];
